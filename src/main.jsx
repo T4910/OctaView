e661ceda7801c480a4fe37eff6c937cf2936ed0e
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-10 18:20:10 +0100

    working on the frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
 import './index.css'
+//? existing imports
+import Root from './routes/root'
+import ErrorPage from './Error-page'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/login',
+    element: <div>Login page under construction</div>
+  },
+  {
+    path: '/view-timetable',
+    element: <div>View time table page under construction</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>,
 )
