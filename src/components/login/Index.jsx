4dd78bc7c9933168d2443e97bef01b4e602fb8a6
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-11 18:19:28 +0100

    working on authenticating user

--- src/components/login/Index.jsx
+++ src/components/login/Index.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+//! import css
+import styles from './Index.module.css'
 
 const serverLink = import.meta.env.VITE_SERVER_LINK
 
 function Login() {
-    
+    const navigate = useNavigate()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setIsLoading(true)
         const response = await axios.post(`${serverLink}/login`, { email, password })
-        console.log(response);
+        if (response) {
+            if (response.status === 200) {
+                if (response.data.role === 'faculty') {
+                    navigate('/')
+                    console.log('faculty');
+                } else if (response.data.role === 'staff') {
+                    navigate('/')
+                    console.log('staff');
+                }
+            }
+            setIsLoading(false)
+        }
     }
 
     return (
@@ -24,7 +41,7 @@ function Login() {
                 <label>Password</label>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <br />
-                <button type="submit">Login</button>
+                <button type="submit" id={isLoading ? styles.submit : ''} disabled={isLoading}>Login</button>
             </form>
         </div>
     )
