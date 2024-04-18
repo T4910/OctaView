0afa421553bb62826fcee2d0716d9dea3416cbb9
T4910 <taiwo.emmanuel@lmu.edu.ng>
2024-04-18 21:33:54 +0100

    [UPDATE] added page to create new timetable

--- src/components/admin/new-timetable-config/Index.jsx
+++ src/components/admin/new-timetable-config/Index.jsx
@@ -2,12 +2,14 @@ import TimeTable from '@/components/timetable'
 import Details from '@/components/admin/new-timetable-config/details'
 import { useState } from 'react';
 
+// TODO: 
+// Cool loading component that covers the timetable form creator 
 
 const Index = () => {
     const [schedule, setSchedule] = useState({ startDay: null, endDay: null, lectureHours: null, departments: null, mode: null });
 
     return (
-        <div className='min-w-fit mx-auto mt-10 max-w-[50rem] space-y-4'>
+        <div className='min-w-fit mx-auto mt-10 md:via-slate-50 lg:via-white sm: max-w-[50rem] space-y-4'>
             <div className="mx-auto grid w-full max-w-6xl gap-1">
                 <h1 className="text-xl font-semibold">Create new timetable</h1>
                 <div>
@@ -15,8 +17,7 @@ const Index = () => {
                     <p>Want to start from an existing timetable template? <span className='underline text-blue-500'>Import previous timetables</span></p>
                 </div>
             </div>
-            <Details />
-            <TimeTable schedule={schedule}/>
+            {!!schedule.startDay ? <TimeTable schedule={schedule}/> : <Details />}
         </div>
     )
 };
